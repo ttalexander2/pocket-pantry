b942bbbbf1fc32b56ceac6da943620c7800cfd92
Thomas Alexander <ttalexander2@email.arizona.edu>
2020-10-01 16:17:51 -0700

    Setup react-native project

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, '../client/web-build')));
+
+// Once client is built in production
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '../client/web-build/index.html'), function (err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    });
+  })
+
+app.listen(port, () => {
+  console.log(`Pocket pantry running at http://localhost:${port}`)
+})
