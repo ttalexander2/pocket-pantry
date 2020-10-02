3fdbea1550ebb407d65aa5b84169130b06cbf6d1
Thomas Alexander <ttalexander2@email.arizona.edu>
2020-10-01 18:51:27 -0700

    changed message

--- Client/App.js
+++ Client/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi Grace!</Text>
+      <Text>Welcome to Pocket Pantry!</Text>
       <StatusBar style="auto" />
     </View>
   );
