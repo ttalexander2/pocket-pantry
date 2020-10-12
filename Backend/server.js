014525f4685a7a7aca392b719e886642b0dc3f77
Thomas Alexander <ttalexander2@email.arizona.edu>
2020-10-11 19:37:55 -0700

    moved db functions into separate module

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,19 @@
-const mariadb = require('mariadb');
 const express = require('express');
 const path = require('path');
+const db = require('./Database')
 const app = express()
 const port = 42069
-const pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'BigDaddy',
-  connectionLimit: 5
-});
+
 
 
 app.use(express.static(path.join(__dirname, '../client/web-build')));
-insertFDAInfo(1234, "test", "ur mom", new Date());
-insertIngredientInfo(1234, "test", "ur mom", 6.9, "tbsp", new Date(), new Date());
-insertMealInfo("mush", 2, new Date());
+db.insertFDAInfo(1234, "test", "ur mom", new Date());
+db.insertIngredientInfo(1234, "test", "ur mom", 6.9, "tbsp", new Date(), new Date());
+db.insertMealInfo("mush", 2, new Date());
 
-deleteIngredientInfo(1234);
-deleteFDAInfo(1234);
-deleteMealInfo("mush");
+//deleteIngredientInfo(1234);
+//deleteFDAInfo(1234);
+//deleteMealInfo("mush");
 
 // Once client is built in production
 app.get('/', (req, res) => {
@@ -33,92 +28,3 @@ app.listen(port, () => {
     console.log(`Pocket pantry running at http://localhost:${port}`)
 })
 
-async function insertFDAInfo(upc, name, brand, expiration) {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const res = await conn.query("USE pantry");
-    
-    res = await conn.query("INSERT INTO FDARecommendations value (?, ?, ?, ?)", [upc, name, brand, expiration]);
-    
-  } catch (err) {
-    console.log(err);
-  } finally {
-    if (conn) conn.release();
-  }
-}
-
-async function deleteFDAInfo(upc) {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const res = await conn.query("USE pantry");
-    
-    res = await conn.query("DELETE FROM FDARecommendations WHERE upc='" + upc + "'");
-    
-  } catch (err) {
-    console.log(err);
-  } finally {
-    if (conn) conn.release();
-  }
-}
-
-async function insertIngredientInfo(upc, name, brand, amount, unitOfAmount, expiration, datePurchased) {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const res = await conn.query("USE pantry");
-    
-    res = await conn.query("INSERT INTO Ingredients(upc,name,brand,amount,unitOfAmount,expirationDate,dateOfPurchase) value (?, ?, ?, ?, ?, ?, ?)", [upc, name, brand, amount, unitOfAmount, expiration, datePurchased]);
-    
-  } catch (err) {
-    console.log(err);
-  } finally {
-    if (conn) conn.release();
-  }
-}
-
-async function deleteIngredientInfo(upc) {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const res = await conn.query("USE pantry");
-    
-    res = await conn.query("DELETE FROM Ingredients WHERE upc='" + upc + "'");
-    
-  } catch (err) {
-    console.log(err);
-  } finally {
-    if (conn) conn.release();
-  }
-}
-
-async function insertMealInfo(name, portions, dateOfCreation) {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const res = await conn.query("USE pantry");
-    
-    res = await conn.query("INSERT INTO Meals(name,portions,dateOfCreation) value (?, ?, ?)", [name, portions, dateOfCreation]);
-    
-  } catch (err) {
-    console.log(err);
-  } finally {
-    if (conn) conn.release();
-  }
-}
-
-async function deleteMealInfo(name) {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const res = await conn.query("USE pantry");
-    
-    res = await conn.query("DELETE FROM Meals WHERE name='" + name + "'");
-   
-  } catch (err) {
-    console.log(err);
-  } finally {
-    if (conn) conn.release();
-  }
-}
