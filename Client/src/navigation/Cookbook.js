5f337cf2c92f2ba3cffa7d243cec48c067daf64c
Thomas Alexander <ttalexander2@email.arizona.edu>
2020-10-11 18:42:36 -0700

    updated client code

--- Client/src/navigation/Cookbook.js
+++ Client/src/navigation/Cookbook.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { ThemeProvider, Header } from 'react-native-elements';
+import HomeBar from './HomeBar';
 
-export default class Calendar extends React.Component{
+export default class Cookbook extends React.Component{
 
 render() {
     return (
       <ThemeProvider>
-      <Header centerComponent={{ text: 'CALENDAR', style: { color: '#fff' } }}/>
+      <HomeBar name='Cookbook' />
       </ThemeProvider>
     );
   }
